965cb2eacf247db700f75d3cbbfc8929683f93cb
lopeslyra10 <augustolopeslyra@gmail.com>
2025-03-19 01:50:50 -0300

    Teste referente as classes Funcionario implementadas no Main

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,7 +1,24 @@
 package br.com.fiap;
 
+import br.com.fiap.entity.*;
+
 public class Main {
     public static void main(String[] args) {
+        FuncionarioInterface func1 = new Funcionario("Augusto", 40, 20);
+        FuncionarioInterface func2 = new FuncionarioSenior("Felipe", 45, 30);
+        FuncionarioInterface func3 = new FuncionarioFreelancer("Vinicius", 30, 25);
+        FuncionarioInterface func4 = new FuncionarioEstagiario("Gustavo", 20, 15);
 
+        System.out.println("\n--- Funcionário Comum ---");
+        func1.imprimirInformacao();
+
+        System.out.println("\n--- Funcionário Sênior ---");
+        func2.imprimirInformacao();
+
+        System.out.println("\n--- Funcionário Freelancer ---");
+        func3.imprimirInformacao();
+
+        System.out.println("\n--- Funcionário Estagiário ---");
+        func4.imprimirInformacao();
     }
 }
