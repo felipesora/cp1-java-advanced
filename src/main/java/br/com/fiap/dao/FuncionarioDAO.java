3202b56576b1546226ca7c6ffc2bba093baeb995
felipesora <felipeusora@gmail.com>
2025-03-19 20:36:38 -0300

    Arrumando erros de importações na interface FuncionarioDAO

--- src/main/java/br/com/fiap/dao/FuncionarioDAO.java
+++ src/main/java/br/com/fiap/dao/FuncionarioDAO.java
@@ -1,6 +1,8 @@
 package br.com.fiap.dao;
 
 import br.com.fiap.entity.Funcionario;
+import br.com.fiap.exception.CommitException;
+import br.com.fiap.exception.IdNaoEncontradoException;
 
 public interface FuncionarioDAO {
     void cadastrar(Funcionario funcionario);
