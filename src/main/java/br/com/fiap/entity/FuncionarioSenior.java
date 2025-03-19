4132db1b31b1605b703b3418896a09644040a078
lopeslyra10 <augustolopeslyra@gmail.com>
2025-03-19 01:49:02 -0300

    Classes FuncionarioEstagiarioSenior criada

---
+++ src/main/java/br/com/fiap/entity/FuncionarioSenior.java
@@ -0,0 +1,23 @@
+package br.com.fiap.entity;
+
+public class FuncionarioSenior extends Funcionario {
+    private double bonusPor15Horas = 100.0;
+
+    public FuncionarioSenior(String nome, double horasTrabalhadas, double valorHora) {
+        super(nome, horasTrabalhadas, valorHora);
+    }
+
+    @Override
+    public double calcularSalario() {
+        double bonus = (int)(horasTrabalhadas / 15) * bonusPor15Horas;
+        return super.calcularSalario() + bonus;
+    }
+
+    @Override
+    public void imprimirInformacao() {
+        super.imprimirInformacao();
+        System.out.println("Bônus recebido: R$ " + ((int)(horasTrabalhadas / 15) * bonusPor15Horas));
+        System.out.println("Salário Final com Bônus: R$ " + calcularSalario());
+    }
+}
+
