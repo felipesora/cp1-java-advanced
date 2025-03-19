9280323eab1af5a2e260d162e2d525fb425cbcbb
felipesora <felipeusora@gmail.com>
2025-03-19 18:03:02 -0300

    Criando a exception CommitException

---
+++ src/main/java/br/com/fiap/exception/CommitException.java
@@ -0,0 +1,11 @@
+package br.com.fiap.exception;
+
+public class CommitException extends RuntimeException {
+  public CommitException(){
+    super("Erro ao realizar o commit");
+  }
+
+  public CommitException(String message) {
+        super(message);
+    }
+}
